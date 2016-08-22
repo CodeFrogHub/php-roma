7f2f584b52eddf59fee54572416794cb834e6049
Steffen Bahrmann <steffen.bahrmann@codefrog-it.de>
2016-08-22 16:34:40 +0200

    added roman lib added method to deromanize strings to numbers added method to romanize numbers to roman strings added contructor with proof for validaty added add and sub instance method to calculate with roman numbers added specs for roman lib and methods

--- src/Roma.php
+++ src/Roma.php
@@ -1,50 +1,127 @@
 <?php
-class Roma {
 
-  private $value = 0;
-  static $r_no = array("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
-  static $no = array(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
+class Roma
+{
+    private $value = 0;
 
-  public function add($value) {
-    // WHAT IS VALUE
+    const CODES = array(
+        "M" => 1000,
+        "CM" => 900,
+        "D" => 500,
+        "CD" => 400,
+        "C" => 100,
+        "XC" => 90,
+        "L" => 50,
+        "XL" => 40,
+        "X" => 10,
+        "IX" => 9,
+        "V" => 5,
+        "IV" => 4,
+        "I" => 1
+    );
 
-    // ADD VALUE TO CURRENT VALUE
-    return new Roma($this->value + $value);
-  }
+    function __construct($value)
+    {
+        $invalidExeption = new InvalidArgumentException(
+            "Paramter for new Roma have to be an positive Integer, a Roma String or an instance of Roma"
+        );
+        // can be integer
+        // can be String
+        // can be a Roma Instance
+        // if invalid -> throw Exception
+        if (is_int($value) && $value > 0) {
+            $this->value = $value;
+        } elseif (is_string($value)) {
+            $codes = self::romanCodes();
+            for ($i = 0; $i < strlen($value); $i++) {
+                if (in_array($value[$i], $codes) === false) {
+                    throw $invalidExeption;
+                }
+            }
+            $this->value = self::deromanize($value);
+        } elseif ($value instanceof self) {
+            $this->value = $value->getNumber();
+        } else {
+            throw $invalidExeption;
+        }
+    }
 
-	public static function deromanize($nummber) {
+    private static function romanCodes()
+    {
+        $array = array_keys(self::CODES);
+        foreach ($array as $elementKey => $element) {
+            if (strlen($element) === 2) {
+                unset($array[$elementKey]);
+            }
+        }
 
-	}
+        return $array;
+    }
 
-  public static function romanize($number) {
-    $r_no = array("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
-		$no = array(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
-		$roman_number = "";
+    public function getNumber()
+    {
+        return $this->value;
+    }
 
-		for ($i = 0; $i < count($no); $i++) {
-			while ($nummber >= $no[$i]) {
-				$roman_number .= $r_no[$i];
-				$nummber -= $no[$i];
-			}
-		}
-    return $roman_number;
-  }
+    public function getRoman()
+    {
+        return self::romanize($this->value);
+    }
 
-  /*function Nummber($roman_number) {
-		$r_no = array("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
-		$no = array(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
-		$number = "";
-    substr_count();
+    public function add($value)
+    {
+        $value = new Roma($value);
+        $value = $value->getNumber();
 
-		for ($i = 0; $i < count($no); $i++) {
+        return new Roma($this->value + $value);
+    }
 
+    public function sub($value) {
+        $value = new Roma($value);
+        $value = $value->getNumber();
 
-			while ($nummber >= $no[$i]) {
-				$roman_number .= $r_no[$i];
-				$nummber -= $no[$i];
-			}
-		}
-    return $roman_number;
-	}*/
+        return new Roma($this->value - $value);
+    }
+
+    /**
+     * @param $roman
+     * @return int
+     */
+    public static function deromanize($roman)
+    {
+        $val = 0;
+        $lst = 0;
+        for ($i = 0; $i < strlen($roman); $i++) {
+            $c = $roman[$i];
+            if ($lst >= self::CODES[$c]) {
+                $val += $lst;
+            } else {
+                $val += ($lst * -1);
+            }
+            $lst = self::CODES[$c];
+        }
+
+        return $val + $lst;
+    }
+
+    /**
+     * @param $number
+     * @return string
+     */
+    public static function romanize($number)
+    {
+        $roman_number = "";
+
+        $values = array_values(self::CODES);
+        $keys = array_keys(self::CODES);
+        for ($i = 0; $i < count($values); $i++) {
+            while ($number >= $values[$i]) {
+                $roman_number .= $keys[$i];
+                $number -= $values[$i];
+            }
+        }
+        return $roman_number;
+    }
 }
+
 ?>
--- tests/RomaTest.php
+++ tests/RomaTest.php
@@ -1,23 +1,146 @@
 <?php
 
-use Service\Roma;
-
-class RomaTest extends PHPUnit_Framework_TestCase {
+class RomaTest extends PHPUnit_Framework_TestCase
+{
     /**
-     * @dataProvider deromanizeDataProvider
+     * @dataProvider constructorDataProvider
+     * @param $case
      */
-    public function testDeromanize($case, $expected) {
-      $this->assertEquals(ROMA::deromanize($case), $expected);
+    public function testConstructorException($case)
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Roma($case);
     }
 
-    public function deromanizeDataProvider()
+    /**
+     * @dataProvider addDataProvider
+     * @param $value1
+     * @param $value2
+     * @param $add
+     * @param $willFail
+     */
+    public function testAdd($value1, $value2, $add, $willFail)
+    {
+        if($willFail === true) $this->expectException(InvalidArgumentException::class);
+        $roma = new Roma($value1);
+        $result = $roma->add($value2);
+        $this->assertEquals($result->getRoman(), $add);
+    }
+
+    /**
+     * @dataProvider subDataProvider
+     * @param $value1
+     * @param $value2
+     * @param $sub
+     * @param $willFail
+     */
+    public function testSub($value1, $value2, $sub, $willFail)
+    {
+        if($willFail === true) $this->expectException(InvalidArgumentException::class);
+        $roma = new Roma($value1);
+        $result = $roma->sub($value2);
+        $this->assertEquals($result->getRoman(), $sub);
+    }
+
+    /**
+     * @dataProvider romansDataProvider
+     * @param $case
+     * @param $expected
+     */
+    public function testGetNumber($case, $expected)
+    {
+        $roma = new Roma($case);
+        $this->assertEquals($roma->getNumber(), $expected);
+    }
+
+    /**
+     * @dataProvider romansDataProvider
+     * @param $expected
+     * @param $case
+     */
+    public function testGetRoman($expected, $case)
+    {
+        $roma = new Roma($case);
+        $this->assertEquals($roma->getRoman(), $expected);
+    }
+
+    /**
+     * @dataProvider romansDataProvider
+     * @param $case
+     * @param $expected
+     */
+    public function testDeromanize($case, $expected)
+    {
+        $this->assertEquals(Roma::deromanize($case), $expected);
+    }
+
+    /**
+     * @dataProvider romansDataProvider
+     * @param $expected
+     * @param $case
+     */
+    public function testRomanize($expected, $case)
+    {
+        $this->assertEquals(Roma::romanize($case), $expected);
+    }
+
+    /**
+     * Providers
+     */
+    public function constructorDataProvider()
+    {
+        // test with number
+        // test with String
+        // test with Roma
+
+        // test invalid Numbers
+        // test invalid Strings
+        return [
+            // FAILS
+            [0],
+            [-1],
+            ['ASD'],
+            ['ABC'],
+            ['MMR'],
+            ['mCM']
+        ];
+    }
+
+    public function romansDataProvider()
     {
         return [
             ['I', 1],
             ['II', 2],
             ['IV', 4],
-            ['MXM', 1990]
+            ['MCMXC', 1990],
+            ['IX', 9],
+            ['XI', 11],
+            ['MMXVI', 2016]
+        ];
+    }
+
+    public function addDataProvider()
+    {
+        return [
+            [1, 2, 'III', false],
+            [5, 4, 'IX', false],
+            [20, 15, 'XXXV', false],
+            [9, 7, 'XVI', false],
+            [2000, 10, 'MMX', false]
+        ];
+    }
+
+    public function subDataProvider()
+    {
+        return [
+            [2, 1, 'I', false],
+            [1, 2, 'I', true],
+            [5, 4, 'I', false],
+            [20, 15, 'V', false],
+            [9, 7, 'II', false],
+            [2000, 10, 'MCMXC', false]
         ];
     }
 }
+
 ?>
